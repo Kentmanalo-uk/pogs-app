bdf817838cbdbd6dc2f6ec903f1228edbd8d89a9
Kent Rolan O. Manalo <manalo.kenttolan@minsu.edu.ph>
2026-09-16 10:19:28 +0800

    update content

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ function App() {
   return (
     <div style={{
       display: 'flex',
+      flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
       height: '100vh',
-      fontSize: '2rem',
-      fontFamily: 'sans-serif'
+      fontFamily: 'sans-serif',
+      textAlign: 'center'
     }}>
-      pogs is now online
+      <h1 style={{ fontSize: '2rem', margin: 0 }}>Pogs is now online</h1>
+      <p style={{ fontSize: '1.2rem', marginTop: '1rem' }}>Kent Manalo</p>
+      <p style={{ fontSize: '1.2rem', margin: 0 }}>BSIT</p>
     </div>
   )
 }

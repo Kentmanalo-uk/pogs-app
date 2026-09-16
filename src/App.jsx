fadf47f81066ad6a3373dd7f63c709270141e0d1
Kent Rolan O. Manalo <manalo.kenttolan@minsu.edu.ph>
2026-09-16 09:16:57 +0800

    Initial commit - pogs app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontSize: '2rem',
+      fontFamily: 'sans-serif'
+    }}>
+      pogs is now online
+    </div>
+  )
+}
+
+export default App
